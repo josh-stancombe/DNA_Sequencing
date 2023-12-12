e6dcd2d577c20fd7d4dd29e493e62f3abd2b8ed1
Ryan Meredith <rwm@udel.edu>
2023-12-12 14:40:06 -0500

    Correct inaccuracies

--- DNA.java
+++ DNA.java
@@ -7,28 +7,28 @@
   / \   \   / \   \  
  ~   `-~ `-`   `-~ `-
 
-The genome of an organism stores all the genetic information necessary to build and maintain that organism. It is an organism’s complete set of DNA. DNA is composed of a series of nucleotides abbreviated as:
+The genome of an organism stores genetic information necessary to build and maintain that organism. DNA contains a series of nucleotides abbreviated as:
 
     A: Adenine
     C: Cytosine
     G: Guanine
     T: Thymine
 
-So a strand of DNA could look something like ACGAATTCCG.
+A sample sequence from DNA could look something like ATGAATTGA. A group of three nucleotides is called a codon. Codons determine which amino acids will be used to make peptides and proteins.
 
-Write a DNA.java program that determines whether there is a protein in a strand of DNA. A protein has the following qualities:
+Write a DNA.java program that determines whether there is a sequence that encodes a peptide in a strand of DNA. A valid sequence has the following qualities:
 
-    - It begins with a “start codon”: ATG.
-    - It ends with a “stop codon”: TGA.
+    - It begins with a “start codon”. For our purposes, use ATG.
+    - It ends with a “stop codon”. For our purposes, use TGA.
     - In between, each additional codon is a sequence of three nucleotides.
 
 So for example:
 
-    - ATGCGATACTGA is a protein because it has the start codon ATG, the stop codon TGA, and the length is divisible by 3.
+    - ATGCGATACTGA is a valid sequence because it has the start codon ATG, the stop codon TGA, and the length is divisible by 3.
 
-    - ATGCGATAGA is not a protein because the sequence length is not divisible by 3, so the third condition is not satisfied.
+    - ATGCGATAGA is not a valid sequence because the length is not divisible by 3, so the third condition is not satisfied.
 
-Here are the three DNA strands that you are going to use to test your program:
+Here are the three sequences that you are going to use to test your program:
 
     - "ATGCGATACGCTTGA"
     - "ATGCGATACGTGA"
@@ -47,13 +47,13 @@ public class DNA {
     String dna2 = "ATGCGATACGTGA";
     String dna3 = "ATTAATATGTACTGA";
 
-    proteinFinder(dna1);
-    proteinFinder(dna2);
-    proteinFinder(dna3);
+    peptideFinder(dna1);
+    peptideFinder(dna2);
+    peptideFinder(dna3);
 
   }
 
-  public static void proteinFinder(String dna){
+  public static void peptideFinder(String dna){
 
     System.out.println("DNA sample: " + dna);
     int dna_length = dna.length();
@@ -63,14 +63,14 @@ public class DNA {
     // Find index of ATG codon:
     int startInt = dna.indexOf("ATG");
     int endInt = dna.indexOf("TGA");
-    int proteinLength = endInt - startInt;
+    int peptideLength = endInt - startInt;
 
-    // Find Protein:
-    if (startInt != -1 && endInt != -1 && proteinLength % 3 == 0){
-      String protein = dna.substring(startInt, (startInt + 3));
-      System.out.println("- Protein: " + protein + "\n");
+    // Find Peptide:
+    if (startInt != -1 && endInt != -1 && peptideLength % 3 == 0){
+      String peptide = dna.substring(startInt, (startInt + 3));
+      System.out.println("- Peptide: " + peptide + "\n");
     } else {
-      System.out.println("- No Protein Found \n");
+      System.out.println("- No Peptide Found \n");
     }
   }
 
--- README.md
+++ README.md
@@ -1,28 +1,28 @@
 # DNA_Sequencing
 Java Codecademy Project
 
-The genome of an organism stores all the genetic information necessary to build and maintain that organism. It is an organism’s complete set of DNA. DNA is composed of a series of nucleotides abbreviated as:
+The genome of an organism stores genetic information necessary to build and maintain that organism. DNA contains a series of nucleotides abbreviated as:
 
     A: Adenine
     C: Cytosine
     G: Guanine
     T: Thymine
 
-So a strand of DNA could look something like ACGAATTCCG.
+A sample sequence from DNA could look something like ATGAATTGA. A group of three nucleotides is called a codon. Codons determine which amino acids will be used to make peptides and proteins.
 
-Write a DNA.java program that determines whether there is a protein in a strand of DNA. A protein has the following qualities:
+Write a DNA.java program that determines whether there is a sequence that encodes a peptide in a strand of DNA. A valid sequence has the following qualities:
 
-    - It begins with a “start codon”: ATG.
-    - It ends with a “stop codon”: TGA.
+    - It begins with a “start codon”. For our purposes, use ATG.
+    - It ends with a “stop codon”. For our purposes, use TGA.
     - In between, each additional codon is a sequence of three nucleotides.
 
 So for example:
 
-    - ATGCGATACTGA is a protein because it has the start codon ATG, the stop codon TGA, and the length is divisible by 3.
+    - ATGCGATACTGA is a valid sequence because it has the start codon ATG, the stop codon TGA, and the length is divisible by 3.
 
-    - ATGCGATAGA is not a protein because the sequence length is not divisible by 3, so the third condition is not satisfied.
+    - ATGCGATAGA is not a valid sequence because the length is not divisible by 3, so the third condition is not satisfied.
 
-Here are the three DNA strands that you are going to use to test your program:
+Here are the three sequences that you are going to use to test your program:
 
     - "ATGCGATACGCTTGA"
     - "ATGCGATACGTGA"
